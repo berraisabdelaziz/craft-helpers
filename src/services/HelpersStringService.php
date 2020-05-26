04e7f15da2361dd90ad2604cf0d053e10b0bbf4d
Abdelaziz BERRAIS <49982155+berraisabdelaziz@users.noreply.github.com>
2020-05-26 09:31:42 +0200

    Update HelpersStringService.php

--- src/services/HelpersStringService.php
+++ src/services/HelpersStringService.php
@@ -15,6 +15,9 @@ use berraisabdelaziz\crafthelpers\Crafthelpers;
 use Craft;
 use craft\base\Component;
 
+use Stringy\Stringy;
+use Urodoz\Truncate\Truncator;
+
 /**
  * HelpersStringService Service
  *
